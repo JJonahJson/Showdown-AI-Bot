b9ef36a031dd431a643da5e97116ec8bbba7b01e
max <massimo.schembri18@gmail.com>
2019-07-20 10:11:31 +0200

    Add datasource class and requirements file

---
+++ src/protocol/data_source.py
@@ -0,0 +1,45 @@
+from abc import ABC, abstractmethod
+from src.model.pokemontype import PokemonType
+import mysql.connector
+
+
+class AbstractDataSource(ABC):
+
+    @abstractmethod
+    def get_pokemon_by_name(self, name):
+        pass
+
+    @abstractmethod
+    def get_pokemontype_by_name(self, name):
+        pass
+
+
+class DatabaseDataSource(AbstractDataSource):
+
+    def __init__(self):
+        super().__init__()
+        self.db_connection = mysql.connector.connect(
+            host="172.17.0.2",
+            user="",
+            passwd="",
+            database="showdown_db"
+        )
+
+    def get_pokemon_by_name(self, name):
+        cursor = self.db_connection.cursor(prepared=True)
+        parametric_query = "SELECT * FROM Pokemon as pkmn WHERE pkmn.name = %s"
+        cursor.execute(parametric_query, name)
+        result = cursor.fetchall()
+        # TODO: Create and return pokemon object
+
+    def get_pokemontype_by_name(self, name):
+        cursor = self.db_connection.cursor(prepared=True)
+        parametric_query = "SELECT pkmn_type FROM Pokemon as pkmn WHERE pkmn.name = %s"
+        cursor.execute(parametric_query, name)
+        result = cursor.fetchall()
+        # TODO: Return Enum value
+        # TODO: Tests
+        return PokemonType[result[0][0]]
+2
+
+
